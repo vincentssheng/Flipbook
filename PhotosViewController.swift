06a2ef0c1c165fa3c5fc8da31dbe9968857baa86
vincentsheng <shengyeesiow96@gmail.com>
2019-08-24 14:52:19 -0700

    Handle requests

--- PhotosViewController.swift
+++ PhotosViewController.swift
@@ -10,4 +10,10 @@ import UIKit
 
 class PhotosViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        store.fetchInterestingPhotos()
+    }
 }
