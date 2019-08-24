0410a2a446c720c1d63fe1d7603f8a38c0e9b21c
vincentsheng <shengyeesiow96@gmail.com>
2019-08-24 12:21:09 -0700

    photorama basics work

---
+++ PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Sheng Yee Siow on 8/24/19.
+//  Copyright © 2019 Vincent Sheng Siow. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
