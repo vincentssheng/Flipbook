7182b607dda8dde0fb50d70ae85d0792825ec906
vincentsheng <shengyeesiow96@gmail.com>
2019-08-24 22:33:42 -0700

    switched to collection view from imgview

---
+++ PhotoDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Sheng Yee Siow on 8/24/19.
+//  Copyright © 2019 Vincent Sheng Siow. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        return cell
+    }
+}
