2fd8fc40332e0e4aebb4326a6f8eddf10944f05f
vincentsheng <shengyeesiow96@gmail.com>
2019-08-25 11:35:47 -0700

    custom cell with loading spinner added

---
+++ PhotoCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Sheng Yee Siow on 8/25/19.
+//  Copyright © 2019 Vincent Sheng Siow. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    // Spinner and image
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    // spinning states
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        update(with: nil)
+    }
+}
