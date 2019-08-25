275c84c77d54ccc07ddf9b00293f166b4cae3cfc
vincentsheng <shengyeesiow96@gmail.com>
2019-08-24 17:01:40 -0700

    First image loaded

--- PhotoStore.swift
+++ PhotoStore.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotosResult {
     case success([Photo])
@@ -28,7 +38,9 @@ class PhotoStore {
             (data, response, error) -> Void in
             
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
@@ -38,5 +50,33 @@ class PhotoStore {
             return .failure(error!)
         }
         return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        return .success(image)
     }
 }
